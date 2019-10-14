5b2c9b9377118e10bc8a01575e7be75abf6ff848
Gerardo Fernández <gerardo@latteandcode.com>
2019-10-14 17:11:05 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,40 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const supportedPaymentMethods = [
+    {
+      supportedMethods: 'basic-card',
+      data: {
+        supportedNetworks: ['visa', 'mastercard']
+      }
+    }
+  ];
+  const paymentDetails = {
+    total: {
+      label: 'Total',
+      amount:{
+        currency: 'EUR',
+        value: 20
+      }
+    }
+  };
+
+  const doPayment = async () => {
+    const paymentRequest =   new PaymentRequest(
+      supportedPaymentMethods,
+      paymentDetails,
+      {}
+    );
+    const paymentResponse = await paymentRequest.show();
+    paymentResponse.complete('success');
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={doPayment}>Pay</button>
       </header>
     </div>
   );
